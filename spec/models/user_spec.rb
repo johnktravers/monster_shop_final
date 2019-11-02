6a245505be1ffcb3df18b26fb159e4d82950ebde
John Travers <johnk.travers@gmail.com>
2019-11-02 11:06:45 -0600

    Remove address validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,8 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :name}
-    it { should validate_presence_of :address}
-    it { should validate_presence_of :city}
-    it { should validate_presence_of :state}
-    it { should validate_presence_of :zip}
     it { should validate_presence_of :email}
     it { should validate_uniqueness_of :email}
-
-    it { should validate_numericality_of(:zip).only_integer }
-    it { should validate_length_of(:zip).is_equal_to(5) }
   end
 
   describe 'relationships' do
