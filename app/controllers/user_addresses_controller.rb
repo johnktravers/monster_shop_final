ba5d38477b467c141026b838005d90d656f52c05
John Travers <johnk.travers@gmail.com>
2019-11-02 20:05:44 -0600

    Add update action and address strong params

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -6,10 +6,26 @@ class UserAddressesController < ApplicationController
     render_404 unless @address
   end
 
+  def update
+    address = current_user.addresses.find_by(id: params[:id])
+    if address && address.update(address_params)
+      flash[:success] = ['Your address has been successfully updated!']
+      redirect_to '/profile'
+    else
+      flash[:error] = address.errors.full_messages
+      @address = Address.new(address_params)
+      render :edit
+    end
+  end
+
 
   private
 
   def require_user
     render_404 unless current_user
+  end
+
+  def address_params
+    params.permit(:nickname, :address, :city, :state, :zip)
   end
 end
