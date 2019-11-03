996f0729c642b9abdd80cb57d25b5b0eb37bb14b
John Travers <johnk.travers@gmail.com>
2019-11-02 19:53:20 -0600

    Add route to edit a user's address

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,8 @@ Rails.application.routes.draw do
   get   '/profile/orders/:id', to: 'user_orders#show'
   patch '/profile/orders/:id', to: 'user_orders#update'
 
+  get   '/profile/addresses/:id/edit', to: 'user_addresses#edit'
+
   get  '/login',  to: 'sessions#new'
   post '/login',  to: 'sessions#create'
   get  '/logout', to: 'sessions#destroy'
