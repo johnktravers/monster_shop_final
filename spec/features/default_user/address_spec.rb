63d9e27b6b84a42a8f19f3560891b80854241503
John Travers <johnk.travers@gmail.com>
2019-11-02 20:05:04 -0600

    Add tests that user can edit addresses from profile page

---
+++ spec/features/default_user/address_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe 'As a default user on my profile page' do
+  before :each do
+    @user = User.create!(name: 'Gmoney', email: 'user@gmail.com', password: 'password123', password_confirmation: 'password123')
+    @address_1 = @user.addresses.create(nickname: 'Home', address: '123 Lincoln St', city: 'Denver', state: 'CO', zip: '23840')
+    @address_2 = @user.addresses.create(nickname: 'Work', address: '412 Broadway Blvd', city: 'Topeka', state: 'KS', zip: '34142')
+
+    visit '/login'
+    fill_in :email, with: 'user@gmail.com'
+    fill_in :password, with: 'password123'
+    click_button 'Login'
+  end
+
+  it 'can click a link to edit an address' do
+    visit '/profile'
+    within("#address-#{@address_1.id}") { click_link 'Edit Address' }
+
+    expect(current_path).to eq("/profile/addresses/#{@address_1.id}/edit")
+
+    fill_in :nickname, with: 'Mountain Villa'
+    fill_in :address, with: '132 Gold Ln'
+    fill_in :city, with: 'Aspen'
+    fill_in :state, with: 'CO'
+    fill_in :zip, with: '80341'
+
+    click_button 'Update Address'
+
+    expect(current_path).to eq('/profile')
+
+    within "#address-#{@address_1.id}" do
+      expect(page).to have_content('Mountain Villa')
+      expect(page).to have_content('132 Gold Ln')
+      expect(page).to have_content('Aspen')
+      expect(page).to have_content('CO')
+      expect(page).to have_content('80341')
+    end
+  end
+
+  it 'sees flash messages if fields are left blank' do
+    visit "/profile/addresses/#{@address_1.id}/edit"
+
+    fill_in :nickname, with: ''
+    fill_in :address, with: '132 Gold Ln'
+    fill_in :city, with: ''
+    fill_in :state, with: ''
+    fill_in :zip, with: '80341'
+
+    click_button 'Update Address'
+
+    expect(current_path).to eq("/profile/addresses/#{@address_1.id}")
+
+    expect(page).to have_content('Nickname can\'t be blank')
+    expect(page).to have_content('City can\'t be blank')
+    expect(page).to have_content('State can\'t be blank')
+  end
+
+end
