8cacf0f51671a10a9df4e8fb804bdcb2577b2686
John Travers <johnk.travers@gmail.com>
2019-11-04 09:48:41 -0700

    Create module of test helper methods to avoid before each repetition

---
+++ spec/support/test_helpers.rb
@@ -0,0 +1,39 @@
+module TestHelpers
+  def create_user_with_addresses
+    @user = User.create!(name: 'Andy Dwyer', email: 'user@gmail.com', password: 'password123', password_confirmation: 'password123')
+    @address_1 = @user.addresses.create(nickname: 'Home', address: '123 Lincoln St', city: 'Denver', state: 'CO', zip: '23840')
+    @address_2 = @user.addresses.create(nickname: 'Work', address: '412 Broadway Blvd', city: 'Topeka', state: 'KS', zip: '34142')
+  end
+
+  def create_merchants_and_items
+    @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd', city: 'Denver', state: 'CO', zip: 80203)
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+    @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+  end
+
+  def create_user_with_orders
+    create_user_with_addresses
+    create_merchants_and_items
+
+    @order_1 = Order.create!(address_id: @address_1.id)
+    @order_2 = Order.create!(address_id: @address_2.id, status: 0)
+
+    @order_1.item_orders.create!(item_id: @tire.id, price: @tire.price, quantity: 2, status: 1)
+    @order_1.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 1)
+
+    @order_2.item_orders.create!(item_id: @tire.id, price: @tire.price, quantity: 3)
+    @order_2.item_orders.create!(item_id: @pencil.id, price: @pencil.price, quantity: 4)
+    @order_2.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 1)
+  end
+
+  def login_as_default_user
+    visit '/login'
+
+    fill_in :email, with: 'user@gmail.com'
+    fill_in :password, with: 'password123'
+
+    click_button 'Login'
+  end
+end
