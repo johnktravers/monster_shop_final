811d5b5a1862edb930bb7e27499e35c0f8dbb632
John Travers <johnk.travers@gmail.com>
2019-11-04 14:32:28 -0700

    Refactor merchant employee tests using helper methods from test helpers module

--- spec/features/merchant_employees/navigation_spec.rb
+++ spec/features/merchant_employees/navigation_spec.rb
@@ -1,25 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "As a merchant employee" do
-  it "cannot access certain paths" do
-    meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd', city: 'Denver', state: 'CO', zip: 80203)
-    employee = meg.users.create!(name: "Gmoney", address: "123 Lincoln St", city: "Denver", state: "CO", zip: 23840, email: "test@gmail.com", password: "password123", password_confirmation: "password123", role: 1)
+RSpec.describe 'As a merchant employee' do
+  it 'cannot access certain paths' do
+    create_merchants_and_items
+    create_merchant_employees
+    login_as_megs_employee
 
-    visit '/login'
-
-    fill_in :email, with: 'test@gmail.com'
-    fill_in :password, with: 'password123'
-
-    click_button 'Login'
-
-    expect(page).to have_content("Logged in as Gmoney")
+    expect(page).to have_content('Logged in as April Ludgate')
 
     visit '/admin'
-
-    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    expect(page).to have_content('The page you were looking for doesn\'t exist (404)')
 
     visit '/merchant/orders/1'
-
-    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    expect(page).to have_content('The page you were looking for doesn\'t exist (404)')
   end
 end
