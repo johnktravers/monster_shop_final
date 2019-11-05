7e8c9d0b34805b44b4b2e28a1f5cff4144407612
John Travers <johnk.travers@gmail.com>
2019-11-05 00:38:45 -0700

    Add test for instance method to retrieve an address's shipped orders

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe Address, type: :model do
     it { should have_many :orders }
   end
 
+  describe 'instance methods' do
+    it 'shipped orders' do
+      create_user_with_addresses
+      order_1 = @address_1.orders.create!
+      order_2 = @address_1.orders.create!(status: 2)
+      order_3 = @address_1.orders.create!(status: 2)
+
+      expect(@address_1.shipped_orders).to eq([order_2, order_3])
+    end
+  end
 end
