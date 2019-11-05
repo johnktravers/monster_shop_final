3ccea4162dd35c4854f08bdbc45c406d09006461
John Travers <johnk.travers@gmail.com>
2019-11-05 12:52:09 -0700

    Add edit, update, and destroy actions

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -15,9 +15,35 @@ class Merchant::CouponsController < Merchant::BaseController
       flash[:success] = ["You have successfully created a #{@coupon.name} coupon!"]
       redirect_to '/merchant/coupons'
     else
-      flash[:error] = @coupon.errors.full_messages
+      flash.now[:error] = @coupon.errors.full_messages
       render :new
     end
+  end
+
+  def edit
+    @merchant = current_user.merchant
+    @coupon = @merchant.coupons.find_by(id: params[:id])
+    render_404 unless @coupon
+  end
+
+  def update
+    @merchant = current_user.merchant
+    @coupon = @merchant.coupons.find_by(id: params[:id])
+    if @coupon.update(coupon_params)
+      flash[:success] = ["You have successfully updated your #{@coupon.name} coupon!"]
+      redirect_to '/merchant/coupons'
+    else
+      flash.now[:error] = @coupon.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    merchant = current_user.merchant
+    coupon = merchant.coupons.find_by(id: params[:id])
+    coupon.destroy
+    flash[:success] = ["You have successfully deleted your #{coupon.name} coupon!"]
+    redirect_to '/merchant/coupons'
   end
 
 
