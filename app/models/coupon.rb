3aa2b44e310f32d7c5929e55588f81538b236ab4
John Travers <johnk.travers@gmail.com>
2019-11-06 06:18:15 -0700

    Add eligible items, subtotals, discount subtotals, and discount total instance methods

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -16,6 +16,63 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :orders
 
+  def eligible_item(item)
+    merchant_id == item.merchant_id
+  end
+
+  def eligible_items(cart)
+    if cart.class == Order
+      cart.items.find_all { |item| item.merchant_id == merchant_id }
+    else
+      cart.keys.find_all { |item| item.merchant_id == merchant_id }
+    end
+  end
+
+  def cart_subtotals(cart)
+    subtotals = {}
+    cart.each do |item, quantity|
+      subtotals[item] = item.price * quantity
+    end
+    subtotals
+  end
+
+  def order_subtotals(order)
+    subtotals = {}
+    order.item_orders.each do |item_order|
+      subtotals[item_order.item] = item_order.price * item_order.quantity
+    end
+    subtotals
+  end
+
+  def discount_subtotals(cart)
+    if cart.class == Order
+      subtotals = order_subtotals(cart)
+    else
+      subtotals = cart_subtotals(cart)
+    end
+
+    dollar_discount = dollar_off if dollar_off
+
+    eligible_items(cart).each do |item|
+      if percent_off
+        subtotals[item] -= subtotals[item] * percent_off / 100
+      else
+        if dollar_discount >= subtotals[item]
+          dollar_discount -= subtotals[item]
+          subtotals[item] = 0
+        else
+          subtotals[item] -= dollar_discount
+          dollar_discount = 0
+        end
+      end
+    end
+    subtotals
+  end
+
+  def discount_total(cart)
+    discount_subtotals(cart).values.sum
+  end
+
 
   private
 
